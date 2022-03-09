5d02cab39667685027c5a068fd079014ad6d321e
William Barnett <will@xbarnett.org>
2022-03-09 09:37:05 +0000

    make - denote xor

--- src/TruthTable.hs
+++ src/TruthTable.hs
@@ -92,7 +92,7 @@ infix_ops = map (M.fromList . concat) [
   [
   lift_binary "\\land" ["&", "&&", "*", "and"] OP.LeftAssoc And,
   lift_binary "\\lor" ["|", "||", "or"] OP.LeftAssoc Or,
-  lift_binary "\\oplus" ["+", "!=", "xor"] OP.LeftAssoc Xor]]
+  lift_binary "\\oplus" ["+", "-", "!=", "xor"] OP.LeftAssoc Xor]]
 
 fix_braces :: String -> String
 fix_braces "" = ""
